ee17b9d7100df8d43e8e3ab9e37f0956afc2dbea
m p <mtpap@MacBook-Pro.local>
2023-12-04 16:33:58 -0500

    completed challenge 3

--- order.py
+++ order.py
@@ -1,13 +1,11 @@
-from person import Person
-
 class Order:
     def __init__(self, type, person):
         self.type = type
         self.person = person
 
     def to_string(self):
-        return f'{self.person.name} orders: {self.type}'
+        return f'{self.person} orders: {self.type}'
 
 if __name__ == "__main__":
-    order = Order('Coke', Person('John', 'Coke'))
+    order = Order('Coke', 'John')
     print(order.to_string())
--- person.py
+++ person.py
@@ -1,10 +1,13 @@
+from order import Order
+
 class Person:
     def __init__(self, name, favorite_drink):
         self.name = name
         self.favorite_drink = favorite_drink
 
     def my_order(self):
-        return self.favorite_drink
+        new_order = Order(self.favorite_drink, self.name)
+        return new_order.to_string()
 
 if __name__ == "__main__":
     person = Person('John', 'Coke')
